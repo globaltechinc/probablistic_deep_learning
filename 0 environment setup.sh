e34bdc00869e8044a3ac5b6720d1c3e33ca80aa5
Jesse Williams <jwilliams@GLOBALTECH.local>
2024-11-10 11:09:14 -0500

    Update 0 environment setup.sh

---
+++ 0 environment setup.sh
@@ -0,0 +1,18 @@
+virtualenv env
+
+source env/bin/activate
+
+pip install pandas==1.3.3
+pip install scikit-learn==1.0.1
+pip install matplotlib==3.5.0
+pip install seaborn
+pip install notebook==6.4.5
+pip install xgboost==2.1.1
+pip install tensorflow==2.6.1
+pip install git+http://github.com/nikitadurasov/masksembles
+
+
+jupyter notebook
+
+
+deactivate
