7287be7144f3b52b3f4f45dedd7d7a86dc3e69fb
Global Technology Connection Inc. <46505306+globaltechinc@users.noreply.github.com>
2024-12-19 10:22:46 -0500

    Update 0 environment setup.sh

--- 0 environment setup.sh
+++ 0 environment setup.sh
@@ -8,7 +8,7 @@ pip install matplotlib==3.5.0
 pip install seaborn
 pip install notebook==6.4.5
 pip install xgboost==2.1.1
-pip install tensorflow==2.6.1
+pip install keras==2.13.1 tensorflow==2.13.0
 pip install --upgrade numpy==1.23.*
 pip install --upgrade protobuf==3.20.*
 pip install git+http://github.com/nikitadurasov/masksembles
